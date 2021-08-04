299216a67e211b5e5bdae047c096425bcb77ad54
unknown <mohamed202029999@gmail.com>
2021-08-04 22:52:37 +0300

    Adding slider

--- js/main.js
+++ js/main.js
@@ -41,3 +41,77 @@ topSpan.addEventListener('click', () => window.scrollTo({
     top: 0,
     behavior: 'smooth',
 }))
+
+// slider our project
+// git slider imgs, prev, next, pagination
+let imgs = Array.from(document.querySelectorAll('.slider-container-imgs img')),
+    imgLength = imgs.length,
+    prev = document.querySelector('.slider-container-prev'),
+    next = document.querySelector('.slider-container-next'),
+    pagination = document.querySelector('.slider-pagination');
+// create ul list  and  append to pagination
+const listPag = document.createElement('ul');
+// make loop from imgs to create list element and adding to pagination
+for(let i = 1 ;i <= imgLength ;i++){
+    const listPagElem = document.createElement('li');
+    listPagElem.setAttribute('data-nav',i);
+    listPagElem.appendChild(document.createTextNode(i));
+    listPag.appendChild(listPagElem)
+}
+pagination.appendChild(listPag);
+// prev and next function 
+let  currentImg = 1;
+paginationList = Array.from(document.querySelectorAll('.slider-pagination ul li'));
+next.addEventListener('click', ()=>{
+    if(currentImg == imgLength){
+        return  false;
+    }else{
+        currentImg++;
+        checker();
+    }
+});
+prev.addEventListener('click', ()=>{
+    if(currentImg == 1){
+        return  false;
+    }else{
+        currentImg--;
+        checker();
+    }
+
+});
+///
+console.log(paginationList)
+for (let li of paginationList){
+    li.addEventListener('click', ()=>{
+        currentImg = li.getAttribute('data-nav');
+        checker();
+    })
+}
+
+// function checker for the imgs and pagenation
+
+     
+function checker(){
+    // clear all active from imgs and pagination list
+    clearAll();
+    imgs[currentImg - 1].classList.add('active');
+    paginationList[currentImg -1].classList.add('active');
+    if (currentImg == 1  ){
+        prev.classList.add('disabled')
+    }else {
+        prev.classList.remove('disabled')
+    }
+    if (currentImg == imgLength  ){
+        next.classList.add('disabled')
+    }else {
+        next.classList.remove('disabled')
+    }
+}
+function clearAll(){
+    imgs.forEach((img)=>img.classList.remove('active') )
+    paginationList.forEach((li)=>li.classList.remove('active') )
+    
+}
+checker();
+
+// currentImg++;
